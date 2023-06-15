b2298b747293a2798d8ef012e768e5c1ecee03c1
[Kayla Norris] <[kayla.e.norris@gmail.com]>
2023-06-15 13:54:52 -0400

    added resume, changed folder structure, and fixed dependencies

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -5,12 +5,12 @@ export default function About() {
     <div>
       <h1>About Me</h1>
       <p>
-        Nunc pharetra finibus est at efficitur. Praesent sed congue diam.
-        Integer gravida dui mauris, ut interdum nunc egestas sed. Aenean sed
-        mollis diam. Nunc aliquet risus ac finibus porta. Nam quis arcu non
-        lectus tincidunt fermentum. Suspendisse aliquet orci porta quam semper
-        imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus
-        in. Sed rhoncus mollis diam, sit amet facilisis lectus blandit at.
+      I was born in Germany but raised in Columbus, Ohio (GO BUCKS!).<br />
+                I have a background in Veterinary Technology and Life Insurance Sales. However, I've always had a love
+                for all things Design. I enjoy the complexity of making things flow
+                in a zen-like manner. With my previous background, I've gained meticulous organizational skills as well
+                as genuine interpersonal skills. I am very determined and have a self-directed mindset. I look forward to working
+                with you on future projects!
       </p>
     </div>
   );
